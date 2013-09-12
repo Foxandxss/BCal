071829bb254a1e244786bbca08839e4e51693bf7
Jesus Rodriguez <Foxandxss@gmail.com>
2013-09-12 19:25:40 +0200

    Calendar is now a service. First iteration

---
+++ app/js/common/services/calendar.js
@@ -0,0 +1,56 @@
+angular.module('services.calendar', [])
+	.service('calendar', function() {
+		this.getCalendar = function(year, month) {
+			var days = [], numLastDays, numCurrentMonthDays, lastMonthLastDay,
+									lastMonthFirstDay, nextMonthLastDay;
+
+			month = --month; // moment is 0 based
+
+			date = moment([year, month, 1]);
+			// We need to find out how many days we need to fill from the last month
+			switch(date.day()) {
+				case 0: // AKA Sunday, so we need 6 days from the last month
+					numLastDays = 6;
+					break;
+				case 1: // AKA Monday, so we need 7 days from the last month
+					numLastDays = 7;
+					break;
+				default: // If not Monday nor Sunday
+					numLastDays = date.day() - 1;
+			}
+
+			numCurrentMonthDays = date.daysInMonth(); // How many days we have in this month
+			lastMonthLastDay = date.subtract('months', 1).daysInMonth(); // Last day to show in the calendar
+			lastMonthFirstDay = lastMonthLastDay - numLastDays + 1; // First day of the last month
+			nextMonthLastDay = 42 - numCurrentMonthDays - numLastDays; // days we need from the next month
+
+			// Add last month days
+			angular.forEach(_.range(lastMonthFirstDay, lastMonthLastDay + 1), function(day) {
+				days.push({
+					highlight: false,
+					month: 'prev',
+					day: day
+				});
+			});
+
+			// Add current month days
+			angular.forEach(_.range(1, numCurrentMonthDays + 1), function(day) {
+				days.push({
+					highlight: false,
+					month: 'current',
+					day: day
+				});
+			});
+
+			// Add next month days
+			angular.forEach(_.range(1, nextMonthLastDay + 1), function(day) {
+				days.push({
+					highlight: false,
+					month: 'next',
+					day: day
+				});
+			});
+
+			return days;
+		};
+	});
---
+++ spec/services/calendar_spec.js
@@ -0,0 +1,28 @@
+describe("service: calendar", function() {
+	var calendar;
+
+	beforeEach(module("services.calendar"));
+
+	beforeEach(inject(function(_calendar_) {
+		calendar = _calendar_;
+	}));
+
+	describe("#getCalendar(year, month)", function() {
+		it("should generate a calendar for the provided date", function() {
+			var year = 2013, month = 9;
+			var generatedCalendar = calendar.getCalendar(year, month);
+			expect(generatedCalendar.length).toBe(42);
+			expect(_.first(generatedCalendar).day).toBe(26);
+			expect(_.last(generatedCalendar).day).toBe(6);			
+		});
+
+		it("should generate the correct calendar for a leap year", function() {
+			var year = 2012, month = 2; // Leap year
+			var generatedCalendar = calendar.getCalendar(year, month);
+			expect(generatedCalendar.length).toBe(42);
+			expect(_.first(generatedCalendar).day).toBe(30);
+			expect(_.last(generatedCalendar).day).toBe(11);
+		})
+	});
+
+});
